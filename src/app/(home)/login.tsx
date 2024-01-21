03e54890bf29e1d7fb08c361c2b99f0742eebc5e
chichurita <ra.singh069@gmail.com>
2024-01-21 15:00:11 +0100

    redirect correctly (improves devexp)

--- src/app/(home)/login.tsx
+++ src/app/(home)/login.tsx
@@ -4,5 +4,18 @@ import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function Login() {
-  return <Button onClick={() => signIn("github")}>Login with Github</Button>;
+  return (
+    <Button
+      onClick={() =>
+        signIn("github", {
+          callbackUrl:
+            process.env.NODE_ENV === "development"
+              ? "http://localhost:3000/api/auth/callback"
+              : "https://hpi-kicka.vercel.app/api/auth/callback",
+        })
+      }
+    >
+      Login with Github
+    </Button>
+  );
 }
