c46ff8a4160bef34b4e49391df8f3769ea743789
chichurita <ra.singh069@gmail.com>
2024-02-11 02:05:58 +0100

    add link to info page in info card

--- src/app/settings/info.tsx
+++ src/app/settings/info.tsx
@@ -1,15 +1,19 @@
 import {
   Card,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@kicka/components/ui/card";
 
+import Link from "next/link";
+
 export default function Info() {
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="">
         <CardTitle>Information</CardTitle>
+        <CardDescription>Information about the game</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col">
         <code>Version: 0.0.0-alpha</code>
@@ -17,11 +21,16 @@ export default function Info() {
           Feedback:
           <a
             href="mailto:rahul.singh@kickastudent.hpi.de"
-            className="text-wrap text-primary underline-offset-4 hover:cursor-pointer hover:underline"
+            className="text-wrap text-blue-400 text-primary underline-offset-4 hover:cursor-pointer hover:underline"
           >
             rahul.singh@student.hpi.de
           </a>
         </code>
+        <Link href="/info">
+          <code className="text-wrap text-blue-400 text-primary underline-offset-4 hover:cursor-pointer hover:underline">
+            More info here
+          </code>
+        </Link>
       </CardContent>
     </Card>
   );
