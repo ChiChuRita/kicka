ed2b18bc06b3f49ce0f6b611ca8a8d4fc430539f
chichurita <ra.singh069@gmail.com>
2024-02-15 15:27:58 +0100

    smal fix: settings only visible for logged in users

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,11 @@
-"use client";
-
 import ActionSettings from "./account-settings";
 import Header from "@kicka/components/header";
 import Info from "./info";
+import { getSession } from "@kicka/actions/auth";
 
-export default function Settings() {
+export default async function Settings() {
+  await getSession();
+
   return (
     <>
       <Header title="Settings" />
