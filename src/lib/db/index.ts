56be6941f535299db86cadc6dbce5a4cbf897691
chichurita <ra.singh069@gmail.com>
2024-06-22 19:46:45 +0200

    switch to neon db

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,8 @@
 import * as schema from "./schema";
 
-import { drizzle } from "drizzle-orm/vercel-postgres";
-import { sql } from "@vercel/postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.POSTGRES_URL!);
 
 export const db = drizzle(sql, { schema });
