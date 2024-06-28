717317d01296fb51e7ff3d4b47e3930e711bfbe4
chichurita <ra.singh069@gmail.com>
2024-06-28 14:20:50 +0200

    add duo player rating

--- src/lib/skill.ts
+++ src/lib/skill.ts
@@ -1,4 +1,4 @@
-import { Rating, rate_1vs1 } from "ts-trueskill";
+import { Rating, rate_1vs1, rate as rater } from "ts-trueskill";
 import { INITIAL_RATING_MU, INITIAL_RATING_SIGMA } from "./constants";
 
 export class KickaRating {
@@ -27,6 +27,25 @@ export function rate(args: {
   return {
     newWinnerRating: KickaRating.fromRating(newWinner),
     newLoserRating: KickaRating.fromRating(newLoser),
+  };
+}
+
+//TO BE TESTED
+export function rateDuoPlayer(args: {
+  winner0Rating: KickaRating;
+  winner1Rating: KickaRating;
+  loser0Rating: KickaRating;
+  loser1Rating: KickaRating;
+}) {
+  const [[newWinner0, newWinner1], [newLoser0, newLoser1]] = rater(
+    [args.winner0Rating.toRating(), args.winner1Rating.toRating()],
+    [args.loser0Rating.toRating(), args.loser1Rating.toRating()],
+  );
+  return {
+    newWinner0Rating: KickaRating.fromRating(newWinner0),
+    newWinner1Rating: KickaRating.fromRating(newWinner1),
+    newLoser0Rating: KickaRating.fromRating(newLoser0),
+    newLoser1Rating: KickaRating.fromRating(newLoser1),
   };
 }
 
